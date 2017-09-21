63ba920ef4e23b0172fd54c664cb5635e5b64613
velocityzen <velocityzen@gmail.com>
2017-09-20 22:53:27 -0400

    🚀

---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+const Transform = require('stream').Transform;
+const PostCSS = require('postcss');
+
+const Tool = function(fuller, options) {
+  fuller.bind(this);
+
+  const opts = options.postcss;
+  this.map = opts.map;
+  this.processor = PostCSS(this.loadPlugins(opts.plugins));
+};
+
+Tool.prototype = {
+  build: function() {
+    return new Transform({
+      objectMode: true,
+      transform: (mat, enc, next) =>
+        this.process(mat, err => next(err, mat))
+    });
+  },
+
+  process: function(mat, next) {
+    mat.getContent(content => this.processor
+      .process(content.toString(), {
+        from: mat.src.path,
+        to: mat.dst().path,
+        map: this.map
+      })
+      .then(result => {
+        if (result.messages) {
+          result.messages.forEach(message => {
+            if (message.type === 'dependency') {
+              this.addDependencies(message.file, mat.id);
+            }
+          });
+        }
+
+        result.warnings().forEach(warn => this.log(warn.toString()));
+        mat.setContent(result.css);
+
+        if (result.map) {
+          mat.map = result.map;
+        }
+
+        next(null, mat);
+      })
+      .catch(error => {
+        if ( error.name === 'CssSyntaxError' ) {
+          this.error({
+            message: error.reason,
+            file: error.file,
+            line: error.line,
+            column: error.column,
+            extract: error.showSourceCode()
+          });
+        } else {
+          this.error(error);
+        }
+
+        next();
+      })
+    )
+  },
+
+  loadPlugins: function(plugins) {
+    if (!plugins) {
+      throw Error('No postcss plugins are defined');
+    }
+
+    return plugins.map( plugin => {
+      if (typeof plugin === 'string') {
+        return this.require(plugin);
+      }
+
+      let name = Object.keys(plugin)[0];
+      const module = this.require(name);
+      return module(plugin[name]);
+    });
+  }
+};
+
+
+module.exports = Tool;
---
+++ readme.md
@@ -0,0 +1,8 @@
+# PostCSS tool for Fuller #
+**Build everything with right tool**
+
+[For more info about fuller](https://github.com/fullerjs/fuller)
+
+Opens stream from the single file.
+
+For wildcards and multiple files use [src-files tool](https://github.com/fullerjs/fuller-src-files)
